e0232c0dc813fc00dba418634f98beede1250865
Pardivile <rander.pikkani@gmail.com>
2024-11-06 10:34:09 +0200

    kujundus + java algne commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function redirectToBlog() {
     window.location.href = '#blog';
 }
+
+// Select the button element
+const button = document.getElementById('navigateButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+  // Change the URL to navigate to another page
+  window.location.href = 'nextpage.html';  // Replace 'nextpage.html' with the actual page URL
+});
