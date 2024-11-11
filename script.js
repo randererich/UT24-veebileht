e19420f72df0338546b94461a81bf07cc7caa74a
Pardivile <rander.pikkani@gmail.com>
2024-11-11 21:30:09 +0200

    Large commit, prayers in chat

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function redirectToBlog() {
     window.location.href = '#blog';
 }
+<<<<<<< Updated upstream
+=======
+
+const button = document.getElementById('navigateButton');
+
+button.addEventListener('click', function() {
+  window.location.href = 'nextpage.html';
+});
+>>>>>>> Stashed changes
